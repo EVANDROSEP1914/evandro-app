df703c96f0ab91f91b89ec80226484fb91886e88
EVANDROSEP1914 <evandro90ev99@gmail.com>
2026-08-06 10:44:33 -0300

    Rename js/app.js to js/docs/app.js

--- js/app.js
+++
@@ -1,1 +0,0 @@
-const h=new Date().getHours();let t=h<12?'Bom dia':h<18?'Boa tarde':'Boa noite';document.getElementById('greet').textContent=`${t}, Evandro 👋`;if('serviceWorker'in navigator){navigator.serviceWorker.register('service-worker.js');}
